83146d08f33753305cb98a40dfde911399b80af1
Moisés Guilherme <moisescomp@gmail.com>
2020-04-27 23:57:11 -0300

    Heroku

---
+++ src/componentes/Saudacao.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+export default class Saudacao extends Component {
+
+    state = {
+        tipo: this.props.tipo,
+        nome: this.props.nome
+    }
+
+    setTipo(e) {
+        let i = 1
+        this.setState({ tipo: e.target.value })
+    }
+
+    setNome(e) {
+        this.setState({ nome: e.target.value })
+    }
+
+
+    render() {
+        const { tipo, nome } = this.state
+        return (
+            <div>
+                <h1>{tipo} {nome}!</h1>
+                <hr />
+                <input type="text" placeholder="Tipo..." 
+                    value={tipo} onChange={e => this.setTipo(e)}/>
+                <input type="text" placeholder="Nome...."
+                 value={nome}  onChange={e=> this.setNome(e)} />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <div>
+        <Saudacao tipo="Bom dia" nome="Moisés" />        
+    </div>
+, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
